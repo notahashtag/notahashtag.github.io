bf5d414ffdfdd68b609bd286d61d4e78872f16ff
notahashtag <justin.enz+octothorpe@gmail.com>
2026-03-07 18:00:45 -0500

    Added a Tarot page with 1 and 3 card pulls

---
+++ Models/Card.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+public class Card
+{
+    [JsonPropertyName("name_short")]
+    public required string NameShort { get; set; }
+
+    [JsonPropertyName("name")]
+    public required string Name { get; set; }
+
+    [JsonPropertyName("value")]
+    public required string Value { get; set; }
+
+    [JsonPropertyName("value_int")]
+    public int ValueInt { get; set; }
+
+    [JsonPropertyName("type")]
+    [AllowedValues("major", "minor")]
+    public required string Type { get; set; }
+
+    [JsonPropertyName("meaning_up")]
+    public required string MeaningUp { get; set; }
+
+    [JsonPropertyName("meaning_rev")]
+    public required string MeaningDown { get; set; }
+
+    [JsonPropertyName("desc")]
+    public required string Desc { get; set; }
+
+}
---
+++ Models/CardList.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+public class CardList
+{
+    [JsonPropertyName("nhits")]
+    public int NHits { get; set; }
+
+    [JsonPropertyName("cards")]
+    public List<Card>? Cards { get; set; }
+}
